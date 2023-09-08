8bc0d742205f5da76721e3682e7cc88d5b0aeaba
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-08 12:23:07 +0000

    fix route slash

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from Controller.SpaceX import SpaceXLaunches
 from datetime import datetime
 
@@ -15,6 +15,11 @@ def index():
 
 # ================ ALL LAUNCHES DATA ===================
 # v2
+@app.route("/v2/")
+def redirect_to_v2():
+  return redirect(url_for("api_categorize_v2"))
+
+
 @app.route("/v2")
 def api_categorize_v2():
   spacex = SpaceXLaunches()
@@ -23,6 +28,11 @@ def api_categorize_v2():
 
 
 # v3
+@app.route("/v3/")
+def redirect_to_v3():
+  return redirect(url_for("api_categorize_v3"))
+
+
 @app.route("/v3")
 def api_categorize_v3():
   spacex = SpaceXLaunches()
@@ -31,6 +41,11 @@ def api_categorize_v3():
 
 
 # v4
+@app.route("/v4/")
+def redirect_to_v4():
+  return redirect(url_for("api_categorize_v4"))
+
+
 @app.route("/v4")
 def api_categorize_v4():
   spacex = SpaceXLaunches()
@@ -39,6 +54,11 @@ def api_categorize_v4():
 
 
 # v5
+@app.route("/v5/")
+def redirect_to_v5():
+  return redirect(url_for("api_categorize_v5"))
+
+
 @app.route("/v5")
 def api_categorize_v5():
   spacex = SpaceXLaunches()
@@ -47,9 +67,12 @@ def api_categorize_v5():
 
 
 # ================ LAUNCHES CATEGORIZE ================
-
-
 # v4
+@app.route("/v4/launches/")
+def redirect_to_v4_all():
+  return redirect(url_for("api_all_v4"))
+
+
 @app.route("/v4/launches")
 def api_all_v4():
   spacex = SpaceXLaunches()
@@ -58,6 +81,11 @@ def api_all_v4():
 
 
 # v5
+@app.route("/v5/launches/")
+def redirect_to_v5_all():
+  return redirect(url_for("api_all_v5"))
+
+
 @app.route("/v5/launches")
 def api_all_v5():
   spacex = SpaceXLaunches()
@@ -66,11 +94,22 @@ def api_all_v5():
 
 
 # ================ LAUNCHES DETAIL CATGORIZE ===============
+# v4 successful
+@app.route("/v4/launches/successful")
+def redirect_to_v4_successful():
+  return redirect(url_for("api_successful_v4"))
+
+
 @app.route("/v4/launches/successful")
 def api_successful_v4():
   spacex = SpaceXLaunches()
   launches = spacex.successful(spacex.spacex_v4())
   return launches
+
+# v4 failed
+@app.route("/v4/launches/failed/")
+def redirect_to_v4_failed():
+  return redirect(url_for("api_failed_v4"))
 
 
 @app.route("/v4/launches/failed")
@@ -80,11 +119,21 @@ def api_failed_v4():
   return launches
 
 
+# v4 upcoming
+@app.route("/v4/launches/upcoming/")
+def redirect_to_v4_upcoming():
+  return redirect(url_for("api_upcoming_v4"))
+
 @app.route("/v4/launches/upcoming")
 def api_upcoming_v4():
   spacex = SpaceXLaunches()
   launches = spacex.upcoming(spacex.spacex_v4())
   return launches
+
+# v5 successful
+@app.route("/v5/launches/successful/")
+def redirect_to_v5_successful():
+  return redirect(url_for("api_successful_v5"))
 
 
 @app.route("/v5/launches/successful")
@@ -93,12 +142,23 @@ def api_successful_v5():
   launches = spacex.successful(spacex.spacex_v5())
   return launches
 
+# v5 failed
+@app.route("/v5/launches/failed/")
+def redirect_to_v5_failed():
+  return redirect(url_for("api_failed_v5"))
+
 
 @app.route("/v5/launches/failed")
 def api_failed_v5():
   spacex = SpaceXLaunches()
   launches = spacex.failed(spacex.spacex_v5())
   return launches
+
+
+# v5 upcoming
+@app.route("/v5/launches/upcoming/")
+def redirect_to_v5_upcoming():
+  return redirect(url_for("api_upcoming_v5"))
 
 
 @app.route("/v5/launches/upcoming")
